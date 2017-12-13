7478509d4beb11c460c76bbc4ffef1dcfbbb1851
AlexFursenkoOd <andersen.od.ua@gmail.com>
2017-12-13 12:53:16 +0200

    Added mised part of view

--- PrismicPreniew/Controllers/HomeController.cs
+++ PrismicPreniew/Controllers/HomeController.cs
@@ -14,8 +14,36 @@ namespace PrismicPreniew.Controllers
     {
         public async Task<ActionResult> Index()
         {
+            var key = "mycourusel";
+            var cache = new DefaultCache();
+            var imageList = cache.Get(key)?.ToObject<List<IndexViewModel>>();
+            if (imageList == null)
+            {
+                var endpoint = "https://preview.prismic.io/api";
+                var api = await Api.Get(endpoint);
+                var document = await api.GetByUID("carousel", "mycouruselru");
+                var docs = document.GetGroup("carousel.carouselimages").GroupDocs;
 
-            return View();
+                var docIds = docs.Select(doc => ((DocumentLink)doc.Fragments["carouselimage"]).Id.ToString()).ToList();
+                var result = await (api.GetByIDs(docIds, lang: "RU").Submit());
+
+                var images = result.Results.Select(r =>
+                {
+                    return new IndexViewModel
+                    {
+                        Title = r.GetStructuredText("carouselimage.imagetitle").getTitle()?.Text,
+                        OrdinalNumber = r.GetNumber("carouselimage.ordinalnumber")?.Value,
+                        Description = r.GetStructuredText("carouselimage.imagedescription")?.AsHtml(new Resolver()).Replace("<p>", "").Replace("</p>", ""),
+                        ImageUrl = r.GetImageView("carouselimage.image", "main").Url
+                    };
+                }).ToList();
+
+                images[0].Active = true;
+                cache.Set(key, 240000, Newtonsoft.Json.Linq.JToken.FromObject(images));
+                imageList = cache.Get(key).ToObject<List<IndexViewModel>>();
+            }
+
+            return View(imageList);
         }
 
         public async Task<ActionResult> About()
